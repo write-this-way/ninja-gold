3b83bad131711997ba95d4990f4854b0fff4722a
Dana Amihere <lizzy7395@gmail.com>
2017-08-11 14:17:55 -0500

    Added email and birthdate

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import re
 from flask import Flask, render_template, request, session, redirect, flash
+from datetime import datetime, date, time
 app = Flask(__name__)
 app.secret_key = 'keepitonlock'
 
@@ -10,52 +11,81 @@ def home():
 
 
 @app.route('/registration', methods=['POST'])
-
 def register():
+
+    errors = 0
+    email_regex = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+    password_regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$')
+    birthday = datetime.strptime(request.form['birthdate'], "%m/%d/%Y")
+
 
 # First and Last Name cannot contain any numbers
 
-    if len(request.form['firstname']) != 0: # defines a string
+    if any(char.isdigit() for char in request.form['firstname']) == True:
         flash("First name cannot contain a number")
-    elif len(request.form['firstname']) < 1: # has no characters filled in
+        errors += 1
+    elif len(request.form['firstname']) == '': # has no characters filled in
         flash("Please enter a first name")
+        errors += 1
     else:
-        return render_template('registration.html', firstname=session['firstname'], form_data=request.form)
+        session['firstname']=request.form['firstname']
 
-    if len(request.form['lastname']) != 0: # defines a string
+    if any(char.isdigit() for char in request.form['lastname']) == True:
         flash("Last name cannot contain a number")
-    elif len(request.form['firstname']) < 1:
+        errors += 1
+    elif len(request.form['firstname']) == '':
         flash("Please enter a last name") # has no characters filled in
+        errors += 1
     else:
-        return render_template('registration.html', lastname=session['lastname'], form_data=request.form)
+        session['lastname']=request.form['lastname']
 
 # Password should be more than 8 characters
 
     if len(request.form['password']) < 1: # has no characters filled in
-        flash("Password exceeds 8 characters")
+        flash("Password must be at least 8 characters")
+        errors += 1
     elif len(request.form['password']) < 8:
+        errors += 1
         flash("Please enter a password")
     else:
-        return render_template('registration.html', password=session['password'], form_data=request.form)
+        session['password']=request.form['password']
+
+# Birthday must be in the past
+    if request.form['birthdate'] == '':
+        flash('Please enter a birthday')
+        errors += 1
+    elif datetime.now() > birthday:
+        flash('Birthday must be in the past')
+        errors += 1
+    else:
+        session['birthdate']=request.form['birthdate']
 
 # Password and password confirmation should match
 
     if len(request.form['confirmpwd']) != len(request.form['password']):
         flash("Password does not match")
+        errors += 1
+    elif not password_regex.match(request.form['password']):
+        flash("Password must contain at least one lowercase letter, one uppercase letter and one number")
     else:
-        return render_template('registration.html', confirmpwd=session['confirmpwd'], form_data=request.form)
+        session['confirmpwd']=request.form['confirmpwd']
 
 
 # Email should be a valid email
 
-    email = request.form['emailaddress']
-
-    # def isValidEmail(email):
-    if re.match(email) == None:
-		flash("Email address is not valid")
+    if request.form['emailaddress'] == '':
+        flash("Email cannot be blank")
+        errors += 1
+    elif not email_regex.match(request.form['emailaddress']):
+        flash("Not a valid email address")
     else:
-        return render_template('registration.html', emailaddress=session['emailaddress'], form_data=request.form)
+        session['emailaddress']=request.form['emailaddress']
 
 
+    #See if there are any errors
+    if errors > 0:
+        return render_template('index.html')
+    else:
+        return render_template('registration.html')
 
 app.run(debug=True)
