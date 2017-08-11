2e333b628d7aa5f140df93d16cd47eef5e5ae526
Dana Amihere <lizzy7395@gmail.com>
2017-08-11 10:20:07 -0500

    Cleaned up functions

--- server.py
+++ server.py
@@ -12,22 +12,6 @@ def home():
 @app.route('/registration', methods=['POST'])
 
 def register():
-# All fields are required and must not be blank
-    # if len(request.form['firstname']) < 1:
-    #     flash("First name cannot be left blank")
-    # elif len(request.form['lastname']) < 1:
-    #     flash("Last name cannot be left blank")
-    # elif len(request.form['email']) < 1:
-    #     flash("Email cannot be left blank")
-    # elif len(request.form['password']) < 1:
-    #     flash("Password cannot be left blank")
-    # elif len(request.form['confirmpwd']) < 1:
-    #     flash("Please confirm your password")
-    # else:
-    #     # return render_template('index.html', firstname=session['firstname'], lastname=session['lastname'], email=session['email'], password=session['password'], confirmpwd=session['confirmpwd'])
-    #     return render_template('registration.html', form_data=request.form)
-
-    # return redirect('/')
 
 # First and Last Name cannot contain any numbers
 
@@ -62,20 +46,13 @@ def register():
 
 # Email should be a valid email
 
-# email = request.form['email']
-#
-# def isValidEmail(email):
-# 	if len(email) > 7:
-# 		if re.match(request.form['email']) != None:
-# 			return True
-#         else:
-#     	       return False
-#                flash("Email address is not valid")
-#
-# if isValidEmail(email) == True :
-#     print ("This is a valid email address")
-# else:
-# 	print ("This is not a valid email address")
+    email = request.form['emailaddress']
+
+    # def isValidEmail(email):
+    if re.match(email) == None:
+		flash("Email address is not valid")
+    else:
+        return render_template('registration.html', emailaddress=session['emailaddress'])
 
 
 
